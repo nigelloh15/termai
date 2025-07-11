363a40ce8a866909285c92012a263a4d3890eb0e
Nigel <84653162+ShigeoDev@users.noreply.github.com>
2025-07-10 20:30:40 -0400

    initial commit

---
+++ build/CMakeFiles/MyApp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyApp.dir/src/glad.c.o"
+  "CMakeFiles/MyApp.dir/src/glad.c.o.d"
+  "CMakeFiles/MyApp.dir/src/main.cpp.o"
+  "CMakeFiles/MyApp.dir/src/main.cpp.o.d"
+  "MyApp.app/Contents/MacOS/MyApp"
+  "MyApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/MyApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/terminalai.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/terminalai.dir/src/glad.c.o"
+  "CMakeFiles/terminalai.dir/src/glad.c.o.d"
+  "CMakeFiles/terminalai.dir/src/main.cpp.o"
+  "CMakeFiles/terminalai.dir/src/main.cpp.o.d"
+  "terminalai.app/Contents/MacOS/terminalai"
+  "terminalai.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/terminalai.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
